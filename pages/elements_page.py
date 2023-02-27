d8bf1251cad635e65d8a9127ccd277051858d29c
alexa <alexandre.gureev@yandex.ru>
2023-02-27 19:44:33 +0300

    elements_branch

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,5 +1,6 @@
 import time
 
+from generator.generator import generated_person
 from locators.elements_page_locators import TextBoxPageLocators
 from pages.base_page import BasePage
 
@@ -8,16 +9,21 @@ class TextBoxPage(BasePage):
     locators = TextBoxPageLocators()
 
     def fill_all_fields(self):
-        self.element_is_visible(self.locators.FULL_NAME).send_keys('admin')
-        self.element_is_visible(self.locators.EMAIL).send_keys('admin@test.com')
-        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys('test1')
-        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys('test2')
+        person_info = next(generated_person())
+        full_name = person_info.full_name
+        email = person_info.email
+        current_address = person_info.current_address
+        permanent_address = person_info.permanent_address
+        self.element_is_visible(self.locators.FULL_NAME).send_keys(full_name)
+        self.element_is_visible(self.locators.EMAIL).send_keys(email)
+        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys(current_address)
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys(permanent_address)
         self.element_is_visible(self.locators.SUBMIT).click()
+        return full_name, email, current_address, permanent_address
 
     def check_filled_form(self):
-        full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text
-        email = self.element_is_present(self.locators.CREATED_EMAIL).text
-        current_address = self.element_is_present(self.locators.CREATED_CURRENT_ADDRESS).text
-        permanent_address = self.element_is_present(self.locators.CREATED_PERMANENT_ADDRESS)
-        # return full_name, email, current_address, permanent_address
+        full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(':')[1]
+        email = self.element_is_present(self.locators.CREATED_EMAIL).text.split(':')[1]
+        current_address = self.element_is_present(self.locators.CREATED_CURRENT_ADDRESS).text.split(':')[1]
+        permanent_address = self.element_is_present(self.locators.CREATED_PERMANENT_ADDRESS).text.split(':')[1]
         return full_name, email, current_address, permanent_address
