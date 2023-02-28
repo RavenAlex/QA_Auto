b4cf9d114b7b35f68240594a4aa7ad3b4b0d73be
alexa <alexandre.gureev@yandex.ru>
2023-02-28 23:29:52 +0300

    elements_branch

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,4 +1,7 @@
+import random
 import time
+
+from selenium.webdriver.common.by import By
 
 from generator.generator import generated_person
 from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators
@@ -38,5 +41,24 @@ class CheckBoxPage(BasePage):
 
     def click_random_checkbox(self):
         item_list = self.elements_are_visible(self.locators.ITEM_LIST)
-        for item in item_list:
-            print(item.text)
+        # for item in item_list:
+        #     self.go_to_element(item)
+        #     item.click()
+        count = 21
+        while count != 0:
+            item = item_list[random.randint(1, 15)]
+            if count > 0:
+                self.go_to_element(item)
+                item.click()
+                print(item)
+                count -=1
+            else:
+                break
+
+    def get_checked_checkboxes(self):
+        checked_list = self.elements_are_present(self.locators.CHECKED_ITEMS)
+        data = []
+        for box in checked_list:
+            title_item = box.find_elements(By.XPATH, "//ancestor::span[@class='rct-title']")
+            print(title_item.text)
+
