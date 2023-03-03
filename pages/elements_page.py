4506603d38cd2a2217b38014ce2d86f2b6436255
alexa <alexandre.gureev@yandex.ru>
2023-03-03 14:16:20 +0300

    update element test, locators added test radio button

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -4,7 +4,7 @@ import time
 from selenium.webdriver.common.by import By
 
 from generator.generator import generated_person
-from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators
+from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators, RadioButtonPageLocators
 from pages.base_page import BasePage
 
 
@@ -67,4 +67,20 @@ class CheckBoxPage(BasePage):
         return str(data).replace(' ', '').lower()
 
 
+class RadioButtonPage(BasePage):
+    locators = RadioButtonPageLocators()
 
+    def click_on_the_radio_button(self, choice):
+        choices = {'yes': self.locators.YES_RADIOBUTTON,
+                   'impressive': self.locators.IMPRESSIVE_RADIOBUTTON,
+                   'no': self.locators.NO_RADIOBUTTON}
+        self.element_is_visible(choices[choice]).click()
+
+    def get_output_result(self):
+        return self.element_is_present(self.locators.OUTPUT_RESULT).text
+
+    # def checked_rb(self):
+    #     checked_list = self.elements_are_visible(self.locators.CHECKED_RB_LIST)
+    #     item = checked_list[random.randint(1, 2)]
+    #     item.click()
+    #     print(checked_list)
