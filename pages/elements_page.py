72f8ba5191aa879bd6880379764ee1bbf9c7d912
alexa <alexandre.gureev@yandex.ru>
2023-03-14 21:48:38 +0300

    update element test, locators added testes: test_web_table_change_count_row test_web_table_delete_person

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -112,7 +112,7 @@ class WebTablePage(BasePage):
             data.append(item.text.splitlines())
         return data
 
-    def search_some_persone(self, key_word):
+    def search_some_person(self, key_word):
         self.element_is_visible(self.locators.SEARCH_INPUT).send_keys(key_word)
 
     def check_search_person(self):
@@ -120,5 +120,42 @@ class WebTablePage(BasePage):
         row = delete_button.find_element('xpath', self.locators.ROW_PARENT)
         return row.text.splitlines()
 
+    def update_person_info(self):
+        person_info = next(generated_person())
+        age = person_info.age
+        self.element_is_visible(self.locators.UPDATE_BUTTON).click()
+        self.element_is_visible(self.locators.AGE_INPUT).clear()
+        self.element_is_visible(self.locators.AGE_INPUT).send_keys(age)
+        self.element_is_visible(self.locators.SUBMIT_BUTTON).click()
+        return str(age)
 
-# title_item = box.find_element('xpath', self.locators.TITLE_ITEM)
+    def delete_person(self):
+        self.element_is_visible(self.locators.DELETE_BUTTON).click()
+
+    def check_deleted(self):
+        return self.element_is_present(self.locators.NO_ROWS_FOUND).text
+
+    def select_up_to_some_rows(self):
+        count = [5, 10, 20, 25, 50, 100]
+        data = []
+        for x in count:
+            count_row_button = self.element_is_visible(self.locators.COUNT_ROW_LIST)
+            self.go_to_element(count_row_button)
+            count_row_button.click()
+            self.element_is_visible((By.CSS_SELECTOR, f'option[value="{x}"]')).click()
+            data.append(self.check_count_rows())
+        return data
+
+    def check_count_rows(self):
+        list_rows = self.elements_are_present(self.locators.FULL_PEOPLE_LIST)
+        return len(list_rows)
+
+    def remove_footer(self):
+        self.driver.execute_script("document.getElementsByTagName('footer')[0].remove();")
+        print('\nRemove Footer')
+
+    def remove_fixedban(self):
+        self.driver.execute_script("document.getElementById('fixedban').style.display = 'none'")
+        print('\nRemove Fixedban')
+
+
