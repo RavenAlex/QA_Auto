5ab8da50aaf50f86376b0d57459e90e7f4e6ec17
alexa <alexandre.gureev@yandex.ru>
2023-03-19 16:48:14 +0300

    update element test, locators, base_page added testes: test_dynamic_properties test_appear_button test_enable_button

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -2,7 +2,7 @@ import random
 import time
 
 from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonsPage, \
-    UploadAndDownloadPage
+    UploadAndDownloadPage, DynamicPropertiesPage
 
 
 class TestElements:
@@ -116,3 +116,28 @@ class TestElements:
             upload_download_page.open()
             check = upload_download_page.download_file()
             assert check is True, "The file has not been downloaded"
+
+    class TestDynamicProperties:
+
+        def test_dynamic_properties(self, driver):
+            dynamic_properties_page = DynamicPropertiesPage(driver, 'https://demoqa.com/dynamic-properties')
+            dynamic_properties_page.open()
+            color_before, color_after = dynamic_properties_page.check_changed_of_color()
+            assert color_before != color_after, 'Color has not been changed'
+
+        def test_appear_button(self, driver):
+            dynamic_properties_page = DynamicPropertiesPage(driver, 'https://demoqa.com/dynamic-properties')
+            dynamic_properties_page.open()
+            appear = dynamic_properties_page.check_appear_button()
+            assert appear is True, 'Button did not appear after 5 second'
+
+        def test_enable_button(self, driver):
+            dynamic_properties_page = DynamicPropertiesPage(driver, 'https://demoqa.com/dynamic-properties')
+            dynamic_properties_page.open()
+            clickable = dynamic_properties_page.check_enable_button()
+            assert clickable is True, 'Button did not enable after 5 second'
+
+
+
+
+
