77d2ad5bd77858e64b53b549b195c72571d17df3
alexa <alexandre.gureev@yandex.ru>
2023-03-26 21:38:51 +0300

    update element test, locators, base_page added testes: test_auto_complete

--- locators/widgets_page_locators.py
+++ locators/widgets_page_locators.py
@@ -10,3 +10,13 @@ class AccordianPageLocators:
     SECTION_THIRD_CONTENT = (By.CSS_SELECTOR, 'div[id="section3Content"] p')
 
 
+class AutoCompletePageLocators:
+    MULTI_INPUT = (By.CSS_SELECTOR, 'input[id="autoCompleteMultipleInput"]')
+    MULTI_VALUE = (By.CSS_SELECTOR, 'div[class="css-12jo7m5 auto-complete__multi-value__label"]')
+    MULTI_VALUE_REMOVE = (By.CSS_SELECTOR, 'div[class="css-1rhbuit-multiValue auto-complete__multi-value"] svg path')
+    SINGLE_VALUE = (By.CSS_SELECTOR, 'div[class="auto-complete__single-value css-1uccc91-singleValue"]')
+    SINGLE_INPUT = (By.CSS_SELECTOR, 'input[id="autoCompleteSingleInput"]')
+    MULTI_VALUE_REMOVE_ALL = (By.CSS_SELECTOR, 'div[class="auto-complete__indicators css-1wy0on6"]')
+
+
+
--- tests/widgets_test.py
+++ tests/widgets_test.py
@@ -1,4 +1,5 @@
-from pages.widgets_page import AccordianPage
+import time
+from pages.widgets_page import AccordianPage, AutoCompletePage
 
 
 class TestWidgets:
@@ -11,7 +12,45 @@ class TestWidgets:
             first_title, first_content = accordian_page.check_accordian('first')
             second_title, second_content = accordian_page.check_accordian('second')
             third_title, third_content = accordian_page.check_accordian('third')
-            assert first_title == 'What is Lorem Ipsum?' and first_content > 0
-            assert second_title == 'Where does it come from?' and second_content > 0
-            assert third_title == 'Why do we use it?' and third_content > 0
+            assert first_title == 'What is Lorem Ipsum?' and first_content > 0, 'Incorrect title or missing text'
+            assert second_title == 'Where does it come from?' and second_content > 0, 'Incorrect title or missing text'
+            assert third_title == 'Why do we use it?' and third_content > 0, 'Incorrect title or missing text'
+
+
+    class TestAutoComplete:
+
+        def test_auto_complete(self, driver):
+            auto_complete_page = AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
+            auto_complete_page.open()
+            colors = auto_complete_page.fill_input_multi()
+            colors_result = auto_complete_page.check_color_in_multi()
+            print(colors)
+            print(colors_result)
+            assert colors == colors_result, 'TThe added colors are missing in the input'
+
+        def test_remove_value_from_multi(self, driver):
+            auto_complete_page = AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
+            auto_complete_page.open()
+            auto_complete_page.fill_input_multi()
+            count_value_before, count_value_after = auto_complete_page.remove_value_from_multi()
+            print(count_value_before)
+            print(count_value_after)
+            assert count_value_before != count_value_after, 'Value was not deleted'
+
+        def test_fill_single_auto_complete(self, driver):
+            auto_complete_page = AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
+            auto_complete_page.open()
+            color = auto_complete_page.fill_input_single()
+            color_result = auto_complete_page.check_color_in_single()
+            assert color == color_result, 'The added color is missing in the input'
+
+
+        def test_remove_all(self, driver):
+            auto_complete_page = AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
+            auto_complete_page.open()
+            auto_complete_page.fill_input_multi()
+            time.sleep(1)
+            auto_complete_page.check_remove_all_color()
+            time.sleep(2)
+
 
