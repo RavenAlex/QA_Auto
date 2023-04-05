0e4ce9346814392db38f520174fe1b0a4ac9d42c
alexa <alexandre.gureev@yandex.ru>
2023-04-06 00:13:36 +0300

    update element test, locators, base_page added testes: test_selectable

--- pages/interaction_page.py
+++ pages/interaction_page.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-from locators.interaction_page_locators import SortablePageLocators
+from locators.interaction_page_locators import SortablePageLocators, SelectablePageLocators
 from pages.base_page import BasePage
 
 
@@ -35,3 +35,25 @@ class SortablePage(BasePage):
         self.action_drag_and_drop_to_element(item_what, item_where)
         order_after = self.get_sortable_items(self.locators.GRID_ITEM)
         return order_before, order_after
+
+
+class SelectablePage(BasePage):
+
+    locators = SelectablePageLocators()
+
+
+    def click_selectable_items(self, elements):
+        item_list = self.elements_are_visible(elements)
+        random.sample(item_list, k=1)[0].click()
+
+    def click_list(self):
+        self.element_is_visible(self.locators.TAB_LIST).click()
+        self.click_selectable_items(self.locators.LIST_ITEM)
+        active_item = self.element_is_visible(self.locators.LIST_ITEM_ACTIVE)
+        return active_item.text
+
+    def click_grid(self):
+        self.element_is_visible(self.locators.TAB_GRID).click()
+        self.click_selectable_items(self.locators.GRID_ITEM)
+        active_item_grid = self.element_is_visible(self.locators.GRID_ITEM_ACTIVE)
+        return active_item_grid.text
--- tests/interaction_test.py
+++ tests/interaction_test.py
@@ -1,6 +1,6 @@
 import time
 
-from pages.interaction_page import SortablePage
+from pages.interaction_page import SortablePage, SelectablePage
 
 
 class TestInteractions:
@@ -14,3 +14,14 @@ class TestInteractions:
             grid_before, grid_after = sortable_page.change_list_order_grid()
             assert list_before != list_after, 'Elements has not been moved'
             assert grid_before != grid_after, 'Elements has not been moved'
+
+
+    class TestSelectable:
+
+        def test_selectable(self, driver):
+            selectable_page = SelectablePage(driver, 'https://demoqa.com/selectable')
+            selectable_page.open()
+            list_active = selectable_page.click_list()
+            grid_active = selectable_page.click_grid()
+            assert len(list_active) > 0, 'No elements'
+            assert len(grid_active) > 0, 'No elements'
